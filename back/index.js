fcabf1442bb40415b704ce1486af121718875ff6
Eetu Remes <eeturemes@Eetu-MBP.home>
2023-10-26 23:08:43 +0300

    uibuild

--- back/index.js
+++ back/index.js
@@ -1,5 +1,98 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
+const Laulu = require('./models/laulu')
+
+const PORT = process.env.PORT
+
+app.use(morgan('## :method :url -- :status -- :response-time ms -- :body'))
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+
+let laulut = []
 
 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+app.get('/api/laulut', (request, response, next) => {
+    console.log('pöröt')
+    Laulu.find({}).then(laulut => {
+        response.json(laulut)
+    })
+        .catch(error => next(error))
+})
+
+app.post('/api/laulut', (request, response, next) => {
+    const body = request.body
+
+    if (body.nimi === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+    }
+
+    const laulu = new Laulu({
+        nimi: body.nimi,
+        sanat: body.sanat
+    })
+
+    laulu.save()
+        .then(savedLaulu => {
+            response.json(savedLaulu)
+        })
+        .catch(error => next(error))
+})
+
+app.get('/api/laulut/:id', (request, response, next) => {
+    Laulu.findById(request.params.id)
+        .then(laulu => {
+            if (laulu) {
+                response.json(laulu)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/laulut/:id', (request, response, next) => {
+    Laulu.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/laulut/:id', (request, response, next) => {
+    const { nimi, sanat } = request.body
+    console.log(request.body)
+    Laulu.findByIdAndUpdate(
+      request.params.id, 
+      { nimi, sanat },
+      //{ new: true, runValidators: true, context: 'query' }
+    ) 
+      .then(updatedLaulu => {
+        response.json(updatedLaulu)
+      })
+      .catch(error => next(error))
+  })
 
 
-console.log('prööts :D')
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.nimi === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.nimi === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
---
+++ back/models/laulu.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('Yhdistetty MongoDB')
+    })
+    .catch((error) => {
+        console.log('Virhe yhdistäessä tietokantaan:', error.message)
+    })
+
+const lauluSchema = new mongoose.Schema({
+    nimi: {
+        type: String,
+        minlength: 3
+    },
+    sanat: {
+        type: String,
+    }
+})
+
+lauluSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Laulu', lauluSchema)
